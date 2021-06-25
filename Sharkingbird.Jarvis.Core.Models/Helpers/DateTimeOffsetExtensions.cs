80101184c81c566b265ff989390519daf3cc1df4
Jonathan Eccker <jeccker@dealeron.com>
2021-06-25 17:24:42 -0400

    Updated current vacation logic to respect start or end of day, added tests

---
+++ Sharkingbird.Jarvis.Core.Models/Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharkingbird.Jarvis.Core.Models.Helpers
+{
+  public static class DateTimeOffsetExtensions
+  {
+    public static DateTimeOffset StartOfDay(this DateTimeOffset dateTime)
+    {
+      return new DateTimeOffset(
+        dateTime.Year,
+        dateTime.Month,
+        dateTime.Day,
+        0, 0, 0, dateTime.Offset);
+    }
+    public static DateTimeOffset EndOfDay(this DateTimeOffset dateTime)
+    {
+      return new DateTimeOffset(
+        dateTime.Year,
+        dateTime.Month,
+        dateTime.Day,
+        23, 59, 59, dateTime.Offset);
+    }
+  }
+}
---
+++ Sharkingbird.Jarvis.UnitTests/CurrentVacationTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using FakeItEasy;
+using MediatR;
+using NUnit.Framework;
+using Sharkingbird.Jarvis.Core.Models.Vacation;
+
+namespace Sharkingbird.Jarvis.UnitTests
+{
+  public sealed class CurrentVacationTests
+  {
+    [TestCase("Name", 10, 5)]
+    public void TodayAfterEndDateReturnsNoCurrentVacation(string vacationName, int startDaysOffset,
+      int endDaysOffset)
+    {
+      var startDate = DateTimeOffset.Now - TimeSpan.FromDays(startDaysOffset);
+      var endDate = DateTimeOffset.Now - TimeSpan.FromDays(endDaysOffset);
+      var vacation = new Vacation(A.Fake<IMediator>(), Guid.NewGuid(), vacationName, startDate, endDate,
+        new VacationExpense[0]);
+      var budget = new VacationBudget(A.Fake<IMediator>(), 0, 0, new[] { vacation });
+      var currentVacation = budget.CurrentVacation;
+      Assert.Null(currentVacation);
+    }
+    [TestCase("Name",5,10)]
+    public void TodayBeforeStartDateReturnsNoCurrentVacation(string vacationName, int startDaysOffset,
+      int endDaysOffset)
+    {
+      var startDate = DateTimeOffset.Now + TimeSpan.FromDays(startDaysOffset);
+      var endDate = DateTimeOffset.Now + TimeSpan.FromDays(endDaysOffset);
+      var vacation = new Vacation(A.Fake<IMediator>(), Guid.NewGuid(), vacationName, startDate, endDate,
+        new VacationExpense[0]);
+      var budget = new VacationBudget(A.Fake<IMediator>(), 0, 0, new[] { vacation });
+      var currentVacation = budget.CurrentVacation;
+      Assert.Null(currentVacation);
+    }
+    [TestCase("Name",5,10)]
+    public void TodayDuringEndDateReturnsCurrentVacation(string vacationName, int startDaysOffset, int endSecondsOffset)
+    {
+      var startDate = DateTimeOffset.Now - TimeSpan.FromDays(startDaysOffset);
+      var endDate = DateTimeOffset.Now - TimeSpan.FromSeconds(endSecondsOffset);
+      var vacation = new Vacation(A.Fake<IMediator>(), Guid.NewGuid(), vacationName, startDate, endDate,
+        new VacationExpense[0]);
+      var budget = new VacationBudget(A.Fake<IMediator>(), 0, 0, new[] {vacation});
+      var currentVacation = budget.CurrentVacation;
+      Assert.NotNull(currentVacation);
+      Assert.AreEqual(vacationName,currentVacation.Name);
+    }
+
+    [TestCase("Name", 10, 5)]
+    public void TodayDuringStartDateReturnsCurrentVacation(string vacationName, int startSecondsOffset, int endDaysOffset)
+    {
+      var startDate = DateTimeOffset.Now + TimeSpan.FromSeconds(startSecondsOffset);
+      var endDate = DateTimeOffset.Now + TimeSpan.FromDays(endDaysOffset);
+      var vacation = new Vacation(A.Fake<IMediator>(), Guid.NewGuid(), vacationName, startDate, endDate,
+        new VacationExpense[0]);
+      var budget = new VacationBudget(A.Fake<IMediator>(), 0, 0, new[] { vacation });
+      var currentVacation = budget.CurrentVacation;
+      Assert.NotNull(currentVacation);
+      Assert.AreEqual(vacationName, currentVacation.Name);
+    }
+  }
+}
