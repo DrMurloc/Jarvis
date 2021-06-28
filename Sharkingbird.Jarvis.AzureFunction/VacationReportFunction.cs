9c48b2568cf6f561cea2eaabf859bfca90235148
Jonathan Eccker <joneccker@gmail.com>
2021-06-28 01:07:24 -0400

    Added function for sending vacation reports the day after they end

---
+++ Sharkingbird.Jarvis.AzureFunction/VacationReportFunction.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Azure.WebJobs;
+using Sharkingbird.Jarvis.Core.Mediation.Commands;
+using System.Threading.Tasks;
+
+namespace Sharkingbird.Jarvis.AzureFunction
+{
+  public sealed class VacationReportFunction
+  {
+    private readonly IMediator _mediator;
+    public VacationReportFunction(IMediator mediator)
+    {
+      _mediator = mediator;
+    }
+    [FunctionName("VacationReportFunction")]
+    public async Task Run([TimerTrigger("0 45 9 * * *")] TimerInfo myTimer)
+    {
+      await _mediator.Send(new SendVacationReportCommand());
+    }
+  }
+}
